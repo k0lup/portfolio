964bdbda440905063c41fa77f0292498048e80a3
k0lup <cool.k0lup@ya.ru>
2025-12-16 01:40:29 +0300

    Правки ЛР 2.4

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 const menu = {
-    menu: document.querySelector("nav.main-nav"),
-    button: document.querySelector("button.nav-btn"),
+    menu: null,
+    button: null,
+
+    init(selector_menu, selector_button){
+        this.menu = document.querySelector(selector_menu);
+        this.button = document.querySelector(selector_button);
+    },
+
     open(){
         this.menu.classList.remove('main-nav_closed');
         this.button.classList.remove('nav-btn_open');
         this.button.classList.add('nav-btn_close');
         this.button.innerHTML = '<span class="visually-hidden">Закрыть меню</span>';
     },
+
     close(){
         this.menu.classList.add('main-nav_closed');
         this.button.classList.remove('nav-btn_close');
@@ -14,6 +21,8 @@ const menu = {
         this.button.innerHTML = '<span class="visually-hidden">Открыть меню</span>';
     }
 }
+
+menu.init('nav.main-nav', 'button.nav-btn');
 
 menu.close();
 
@@ -26,6 +35,19 @@ menu.button.addEventListener('click', (e) => {
     }
 });
 
+function getComparer(prop) {
+    return function (a, b) {
+        if (a[prop] < b[prop]) {
+            return -1;
+        }
+        
+        if (a[prop] > b[prop]) {
+            return 1;
+        }
+
+        return 0;
+    }
+};
 
 const skills = {
     elem: null,
@@ -36,19 +58,21 @@ const skills = {
         {name:"css", level:40, image:"css.svg"} 
     ],
     sortMode: null,
-    init(text) { this.elem = document.querySelector(text); },
+
+    init(selector) { this.elem = document.querySelector(selector); },
+
     generateList() {
         this.elem.innerHTML = "";
         this.data.forEach((item) => {
             const dt = document.createElement("dt");
-            dt.style.backgroundImage = "url(img/"+item.image+")";
+            dt.style.backgroundImage = `url("img/${item.image}")`;
             dt.className = "skill-item";
             dt.textContent = item.name;
             this.elem.append(dt);
 
             const div = document.createElement("div");
-            div.style.width = item.level + "%";
-            div.textContent = item.level + "%";
+            div.style.width = `${item.level}%`;
+            div.textContent = `${item.level}%`;
 
             const dd = document.createElement("dd");
             dd.className = "skill-level";
@@ -56,30 +80,15 @@ const skills = {
             this.elem.append(dd);
         })
     },
-    getComparer(prop) {
-        return function (a, b) {
-            if (a[prop] < b[prop]) {
-                return -1;
-            }
-            
-            if (a[prop] > b[prop]) {
-                return 1;
-            }
 
-            return 0;
-        }
-    },
     sortList(object_sort) {
         if(this.sortMode === object_sort) {
             this.data.reverse();
-        }
-        else {
-            this.data.sort(this.getComparer(object_sort));
+        } else {
+            this.data.sort(getComparer(object_sort));
             this.sortMode = object_sort;
         }
     }
-
-
 }
 
 skills.init("dl.skill-list");
@@ -107,5 +116,3 @@ skills_sort.addEventListener('click', (e) => {
         }
     }
 });
-
-
